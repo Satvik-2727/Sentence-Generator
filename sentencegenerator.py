faa30b589dd55015d35cf6ed472a6d24f3b7e91d
Satvik <techblogs2021@gmail.com>
2023-05-30 10:49:09 +0530

    Initial commit

---
+++ sentencegenerator.py
@@ -0,0 +1,6 @@
+# In terminal pip install essential-generators
+# In terminal pip install essential-generator
+
+from essential_generators import DocumentGenerator
+gen = DocumentGenerator()
+print(gen.sentence())
